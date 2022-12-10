3a1d978cb47cfe8cb5ebb91f5f2936d7aa73cf8c
Virginia <vbernasconi@altimetrik.com>
2022-12-10 18:36:45 -0300

    add codewars simple code challenges

---
+++ codewars-algoritmos/fundamentals/1.js
@@ -0,0 +1,11 @@
+/* It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters. */
+/* function removeChar(str) {
+  let newArray = str.split('');
+  console.log(newArray);
+  console.log(newArray.shift());
+  newArray.pop();
+  return newArray.join('');
+}
+
+console.log(removeChar('eloquent'));
+ */
